7f0ba9af7b1fcfaaa76083664581b95fe67bfc99
AnshDhunna <157841526+AnshDhunna@users.noreply.github.com>
2024-04-07 02:43:17 -0400

    Create appointment.c

---
+++ appointment.c
@@ -0,0 +1,33 @@
+#include "appointment.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void addAppointment(Appointment **appointments, int *numAppointments) {
+    // Implement adding an appointment
+}
+
+void deleteAppointment(Appointment **appointments, int *numAppointments) {
+    // Implement deleting an appointment
+}
+
+void updateAppointment(Appointment *appointments, int numAppointments) {
+    // Implement updating an appointment
+}
+
+void displaySingleAppointment(Appointment *appointments, int numAppointments) {
+    // Implement displaying a single appointment
+}
+
+void displayRangeAppointments(Appointment *appointments, int numAppointments) {
+    // Implement displaying a range of appointments
+}
+
+void displayAllAppointments(Appointment *appointments, int numAppointments) {
+    // Implement displaying all appointments
+}
+
+Appointment *searchAppointment(Appointment *appointments, int numAppointments, char *keyword) {
+    // Implement searching for an appointment
+    return NULL;
+}
